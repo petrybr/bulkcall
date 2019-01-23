360d095a6eda3d1a77a038b0d4feba77764657d1
Jose Petry <petrybr@gmail.com>
2019-01-23 10:45:39 -0800

    working with the call status of endpoint

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 import getpass
 import csv
+import xmltodict
 
 
 login = raw_input("Login: ")
@@ -12,13 +13,25 @@ print "Abrindo arquivo"
 with open('endpoints.csv', 'r') as arquivo:
     print "Processando arquivo"
     endpoints = csv.DictReader(arquivo)
+    print "Atuando nos endpoints"
+    for endpoint in endpoints:
+        print "Conectando em: " + endpoint["Nome"]
+        with requests.Session() as s:
+            r = s.get('http://' + endpoint["ip"] + '/getxml?location=/Status/SystemUnit/State', auth=(login,passwd))
+        xml = xmltodict.parse(r.text)
+        actives = xml['Status']['SystemUnit']['State']['NumberOfActiveCalls']
+        progress = xml['Status']['SystemUnit']['State']['NumberOfInProgressCalls']
+        suspended = xml['Status']['SystemUnit']['State']['NumberOfSuspendedCalls']
+        total = int(actives) + int(progress) + int(suspended)
+        print "Chamadas ativas: " + actives
+        print "Chamadas em progresso: " + progress
+        print "Chamadas suspensas: " + suspended
+        print "Total de chamadas: " + str(total)
+        if total == 0:
+            print "Ok, you can place a new call"
+        else:
+            print "Endpoitn already in a call, try again later"
 
-print "Atuando nos endpoints"
-for endpoint in endpoints:
-    print "Conectando em: " + endpoint["Nome"]
-    with requests.Session() as s:
-        r = s.get('http://' + endpoint["ip"] + '/getxml?location=/Status/SystemUnit/State', auth=(login,passwd))
-    print r.text
 
 print "Completed!"
 
