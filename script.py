2e0723d502561115529a6eaeee101c5060533fb5
Jose Petry <petrybr@gmail.com>
2019-01-24 11:42:01 -0800

    add requests exceptions

--- script.py
+++ script.py
@@ -19,7 +19,17 @@ with open('endpoints.csv', 'r') as arquivo:
     endpoints = csv.DictReader(arquivo)
     for endpoint in endpoints:
         with requests.Session() as s:
-            r = s.get('http://' + endpoint["ip"] + '/getxml?location=/Status/SystemUnit/State', auth=(login,passwd))
+            try:
+                r = s.get('http://' + endpoint["ip"] + '/getxml?location=/Status/SystemUnit/State', auth=(login,passwd))
+            except requests.ConnectionError as e:
+                print "Connection Error - " + endpoint["Nome"]
+                continue
+            except requests.Timeout as e:
+                print "Timeout Error - " + endpoint["Nome"]
+                continue
+            except requests.RequestException as e:
+                print "General Error - " + endpoint["Nome"]
+                continue
             xml = xmltodict.parse(r.text)
             actives = xml['Status']['SystemUnit']['State']['NumberOfActiveCalls']
             progress = xml['Status']['SystemUnit']['State']['NumberOfInProgressCalls']
@@ -35,10 +45,10 @@ with open('endpoints.csv', 'r') as arquivo:
                 dicttoxml = xmltodict.unparse(mydict)
                 headers = {'Content-Type': 'text/xml'}
                 s.post('http://' + endpoint["ip"] + '/putxml', data=dicttoxml, headers=headers,auth=(login,passwd))
-
             else:
                 print "Endpoint " + endpoint["Nome"] + " already in a call"
 fim = time.time()
 tempo = fim - inicio
 print "Completed in " + str(tempo).split(".")[0] + " seconds!"
 raw_input("Press enter to exit")
+
